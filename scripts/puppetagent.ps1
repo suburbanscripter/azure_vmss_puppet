fcdf3230539f7242e6fdfc34477f96536153cc02
suburbanscripter <suburban.scripter@gmail.com>
2018-06-25 11:29:02 +0300

    initial commit
    initial commit

--- scripts/puppetagent.ps1
+++ scripts/puppetagent.ps1
@@ -1,15 +1,1 @@
-# URL of latest puppet agent for windows:
-# https://downloads.puppetlabs.com/windows/puppet/puppet-agent-x64-latest.msi
-
-# Puppet Master Server
-$puppetserver = "puppetmaster-35b5de4b.westeurope.cloudapp.azure.com"
-
-# Download puppet installer
-$puppet_url = "https://downloads.puppetlabs.com/windows/puppet/puppet-agent-x64-latest.msi"
-$puppet_installer = "C:\puppet_agent.msi"
-
-$client = new-object System.Net.WebClient
-$client.DownloadFile($puppet_url,$puppet_installer)
-
-# Install puppet agent
-msiexec /qn /norestart /i $puppet_installer PUPPET_MASTER_SERVER=$puppetserver
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://puppetmaster.rdstest.local:8140/packages/current/install.ps1', 'install.ps1'); .\install.ps1
